c00a739de492a5642000482908b74941a6cf63c9
kazyu0 <kazumakimijima.sb@gmail.com>
2025-02-26 12:25:09 +0900

    編集・削除

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :destroy]
+
   def index
     @tasks = Task.all.order(:due_date)
   end
@@ -16,7 +18,28 @@ class TasksController < ApplicationController
     end
   end
 
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to root_path, notice: "タスクを更新しました！"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to root_path, notice: "タスクを削除しました！", status: :see_other
+  end
+
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :description, :due_date, :completed)
